e6bae79ffb00bffdb1980ad9ea42a1bd05400ee1
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-15 23:04:42 +0530

    new file:   redteam_lab/lab_env/terraform/c2_infra.tf

---
+++ redteam_lab/lab_env/terraform/c2_infra.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "c2_server" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "Simulated-C2-Server"
+  }
+
+  # OPSEC: Randomize server attributes
+  user_data = <<-EOF
+              #!/bin/bash
+              hostnamectl set-hostname $(openssl rand -hex 4)
+              EOF
+}
