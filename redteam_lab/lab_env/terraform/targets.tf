fc72027552ed3ce6d239789d556e5dd2e2c6e803
pawanchandraupreti <upretipawanchandra@gmail.com>
2025-08-18 23:13:23 +0530

    new file:   redteam_lab/lab_env/terraform/targets.tf

---
+++ redteam_lab/lab_env/terraform/targets.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "victim" {
+  count         = 3
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.micro"
+  
+  tags = {
+    Name = "Victim-${count.index}"
+    Role = "target"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y curl net-tools
+              EOF
+}
